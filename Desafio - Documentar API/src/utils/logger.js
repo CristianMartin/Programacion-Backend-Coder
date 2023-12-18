a929e40a29e2177ead71276f8839b93d35c66ca3
Cristian Martin <c.knoxville7@gmail.com>
2023-12-18 19:42:16 -0300

    update

---
+++ Desafio - Documentar API/src/utils/logger.js
@@ -0,0 +1,67 @@
+import winston from 'winston';
+
+const customLevelOptions = {
+    levels: {
+        fatal: 0,
+        error: 1,
+        warning: 2,
+        info: 3,
+        http: 4,
+        debug: 5
+    },
+    colors: {
+        fatal: 'red',
+        error: 'yellow',
+        warning: 'magenta',
+        info: 'blue',
+        debug: 'white'
+    }
+}
+
+const logger = winston.createLogger({
+    levels: customLevelOptions.levels,
+    transports: [
+        new winston.transports.File({
+            filename: './errors.html',
+            level: 'fatal',
+            format: winston.format.combine(
+                winston.format.simple()
+            )
+        }),
+        new winston.transports.File({
+            filename: './errors.html',
+            level: 'error',
+            format: winston.format.combine(
+                winston.format.simple()
+            )
+        }),
+        new winston.transports.File({
+            filename: './loggers.html',
+            level: 'warning',
+            format: winston.format.combine(
+                winston.format.simple()
+            )
+        }),
+        new winston.transports.File({
+            filename: './loggers.html',
+            level: 'info',
+            format: winston.format.combine(
+                winston.format.simple()
+            )
+        }),
+        new winston.transports.Console({
+            level: 'debug',
+            format: winston.format.combine(
+                winston.format.colorize({ colors: customLevelOptions.colors }),
+                winston.format.simple()
+            )
+        })
+    ]
+})
+
+
+export const addLogger = (req, res, next) => {
+    req.logger = logger
+    req.logger.debug(`${req.method} es ${req.url} - ${new Date().toLocaleTimeString()}`)
+    next()
+}
