e9dd6edbdc7b8fa172f6cae110286a872a71016e
Cristian Martin <c.knoxville7@gmail.com>
2024-01-28 15:04:23 -0300

    update

--- Desafio - Reestructura de nuestro servidor/src/public/js/login.js
+++ Desafio - Reestructura de nuestro servidor/src/public/js/login.js
@@ -1,32 +1,35 @@
 const login = document.getElementById('login');
 
-login.addEventListener('submit', event => {
+login.addEventListener('submit', async event => {
 
     event.preventDefault();
 
     const datForm = new FormData(event.target);
     const loginData = Object.fromEntries(datForm);
 
-    fetch('/api/session/login', {
+    const response = await fetch('/api/session/login', {
         method: 'POST',
         body: JSON.stringify(loginData),
         headers: {
             'Content-type': 'application/json'
         }
-    }).then(result => {
-        if (result.status == 200) {
-            Swal.fire({
-                icon: 'success',
-                title: 'Login exitoso'
-            })
-            setTimeout(function() {location.replace('/');}, 600);
-
-        } else {
-            Swal.fire({
-                icon: 'error',
-                title: 'Error al realizar el login',
-                text: result.error
-            })
-        }
     })
+
+    if (response.status == 200) {
+        const datos = await response.json();
+        console.log(datos);
+        document.cookie = `jwtCookie=${datos.token}; expires=${new Date(Date.now() + 1 * 24 * 60 * 60 * 1000).toUTCString()};path=/;httpOnly=true;`
+        Swal.fire({
+            icon: 'success',
+            title: 'Login exitoso'
+        })
+        setTimeout(function() {location.replace('/');}, 600);
+
+    } else {
+        Swal.fire({
+            icon: 'error',
+            title: 'Error al realizar el login',
+            text: result.error
+        })
+    }
 })
