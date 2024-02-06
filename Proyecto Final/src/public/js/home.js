cfbfd6bf73a2f38b1ae34a9b42ffaa4774b17b1d
Cristian Martin <c.knoxville7@gmail.com>
2024-02-06 01:26:03 -0300

    fix cart

--- Proyecto Final/src/public/js/home.js
+++ Proyecto Final/src/public/js/home.js
@@ -1,36 +1,42 @@
-const addProdToCart = document.getElementById('addProdToCart');
-
-addProdToCart.addEventListener('submit', async event => {
+const addProdToCart = async (event) => {
     event.preventDefault();
-    
     const datForm = new FormData(event.target);
     const data = Object.fromEntries(datForm);
-    const quantity = parseInt(data.quantity)
 
-    const response = await fetch(`/api/carts/${data.cartId}/product/${data.prodId}`, {
-        method: 'POST',
-        body: JSON.stringify({
-            quantity: quantity
-        }),
-        headers: {
-            'Content-type': 'application/json'
+    if(data.login != '/') {
+        const quantity = parseInt(data.quantity)
+
+        const response = await fetch(`/api/carts/${data.cartId}/product/${data.prodId}`, {
+            method: 'POST',
+            body: JSON.stringify({
+                quantity: quantity
+            }),
+            headers: {
+                'Content-type': 'application/json'
+            }
+        })
+
+        if (response.status == 200) {
+            Swal.fire({
+                icon: "success",
+                title: "¡Producto agregado al carrito correctamente!",
+                showConfirmButton: false,
+                timer: 1500,
+            });
+            setTimeout(function() {location.replace('/cart');}, 600);
+
+        } else {
+            Swal.fire({
+                icon: 'error',
+                title: 'Error al agregar el producto en el carrito',
+                text: response.statusText
+            })
         }
-    })
-
-    if (response.status == 200) {
-        Swal.fire({
-            icon: "success",
-            title: "¡Producto agregado al carrito correctamente!",
-            showConfirmButton: false,
-            timer: 1500,
-          });
-        setTimeout(function() {location.replace('/cart');}, 600);
-
     } else {
         Swal.fire({
             icon: 'error',
-            title: 'Error al agregar el producto en el carrito',
-            text: response.statusText
+            title: 'Usuario no logueado',
+            text: 'Debe loguearse para agregar productos a su carrito'
         })
     }
-});
+}
